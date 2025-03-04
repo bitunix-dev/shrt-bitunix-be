d40d433513353af570158748603be5a6eba4eb8b
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2025-03-04 16:51:46 +0700

    add welcome route to web.php for homepage access

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UrlController;
 Route::get('/r/{shortLink}', [UrlController::class, 'redirect'])->name('url.redirect');
+Route::get('/', function () {
+  return view('welcome');
+});
