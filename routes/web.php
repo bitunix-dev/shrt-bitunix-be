0c1af71f90f1c6df45d3a485305c71fcf3243886
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2025-03-06 14:25:03 +0700

    update URL redirect route to remove prefix for short links

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UrlController;
-Route::get('/r/{shortLink}', [UrlController::class, 'redirect'])->name('url.redirect');
+Route::get('/{shortLink}', [UrlController::class, 'redirect'])->name('url.redirect');
 Route::get('/', function () {
   return view('welcome');
 });
