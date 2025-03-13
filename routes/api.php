5d9209ec1a2280101f05f1d6f9a9e2608a871676
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2025-03-13 17:21:40 +0700

    refactor API routes to group authenticated endpoints for better organization

--- routes/api.php
+++ routes/api.php
@@ -8,27 +8,29 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ClickAnalyticsController;
 use App\Http\Controllers\API\AuthController;
 
-Route::apiResource('urls', UrlController::class);
 Route::get('/redirect/{shortLink}', [UrlController::class, 'redirect']);
-Route::apiResource('sources', SourceController::class);
-Route::apiResource('mediums', MediumController::class);
-Route::apiResource('tags', TagController::class);
 Route::get('/qr/{id}', [UrlController::class, 'getQrCode']);
-
-Route::get('/analytics/clicks', [ClickAnalyticsController::class, 'getAllClicks']);
-Route::get('/analytics/clicks/{id}', [ClickAnalyticsController::class, 'getClicksByUrl']);
-Route::get('/analytics/countries', [ClickAnalyticsController::class, 'getClicksByCountry']);
-Route::get('/analytics/cities', [ClickAnalyticsController::class, 'getClicksByCity']);
-Route::get('/analytics/regions', [ClickAnalyticsController::class, 'getClicksByRegion']);
-Route::get('/analytics/continents', [ClickAnalyticsController::class, 'getClicksByContinent']);
-Route::get('/analytics/source', [ClickAnalyticsController::class, 'getClicksBySource']);
-Route::get('/analytics/medium', [ClickAnalyticsController::class, 'getClicksByMedium']);
-Route::get('/analytics/campaign', [ClickAnalyticsController::class, 'getClicksByCampaign']);
-Route::get('/analytics/term', [ClickAnalyticsController::class, 'getClicksByTerm']);
-Route::get('/analytics/content', [ClickAnalyticsController::class, 'getClicksByContent']);
-Route::get('/analytics/devices', [ClickAnalyticsController::class, 'getClicksByDevice']);
-Route::get('/analytics/browsers', [ClickAnalyticsController::class, 'getClicksByBrowser']);
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->put('update-name', [AuthController::class, 'updateName']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('urls', UrlController::class);
+    Route::apiResource('sources', SourceController::class);
+    Route::apiResource('mediums', MediumController::class);
+    Route::apiResource('tags', TagController::class);
+    Route::get('/analytics/clicks', [ClickAnalyticsController::class, 'getAllClicks']);
+    Route::get('/analytics/clicks/{id}', [ClickAnalyticsController::class, 'getClicksByUrl']);
+    Route::get('/analytics/countries', [ClickAnalyticsController::class, 'getClicksByCountry']);
+    Route::get('/analytics/cities', [ClickAnalyticsController::class, 'getClicksByCity']);
+    Route::get('/analytics/regions', [ClickAnalyticsController::class, 'getClicksByRegion']);
+    Route::get('/analytics/continents', [ClickAnalyticsController::class, 'getClicksByContinent']);
+    Route::get('/analytics/source', [ClickAnalyticsController::class, 'getClicksBySource']);
+    Route::get('/analytics/medium', [ClickAnalyticsController::class, 'getClicksByMedium']);
+    Route::get('/analytics/campaign', [ClickAnalyticsController::class, 'getClicksByCampaign']);
+    Route::get('/analytics/term', [ClickAnalyticsController::class, 'getClicksByTerm']);
+    Route::get('/analytics/content', [ClickAnalyticsController::class, 'getClicksByContent']);
+    Route::get('/analytics/devices', [ClickAnalyticsController::class, 'getClicksByDevice']);
+    Route::get('/analytics/browsers', [ClickAnalyticsController::class, 'getClicksByBrowser']);
+    Route::put('update-name', [AuthController::class, 'updateName']);
+});
